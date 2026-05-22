a1f02a2ae8d74e540cdcc8ba136c017c25b8c7f6
# <19975995435@163.com>
2026-05-22 21:48:57 +0800

    fix tests, restore CI; document remaining warnings as accepted (non-ASCII prompts)

---
+++ R/scAgentKit-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+  "_PACKAGE"
+  
+  ## usethis namespace: start
+  #' @importFrom stats setNames
+  #' @importFrom utils head tail
+  #' @importFrom magrittr %>%
+  ## usethis namespace: end
+  NULL
